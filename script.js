24a69be58be8ec7ce28f41b613b9f82dbb74cbf1
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-01-02 17:08:11 +0000

    Created variables and assigned them to HTML ID using the DOM. + Created a syncCharacterFunction  and have synced the character slider input to the character number input

--- script.js
+++ script.js
@@ -2,6 +2,30 @@
 
 
 // When 'Number of Characters' range slider is adjusted. Then the number of characters display is synced accordingly. 
+
+const characterAmountRange = document.getElementById('characterRange')
+const characterAmountNumber = document.getElementById('characterNumber')
+
+characterAmountRange.addEventListener('input', syncCharacters)
+characterAmountNumber.addEventListener('input', syncCharacters)
+
+function syncCharacters(e){
+    let value = e.target.value
+
+    characterAmountRange.value = value
+    characterAmountNumber.value = value
+}
+
+
+// create a value variable
+// how to set value variable = input
+
+// set character amount range.value = value variable
+// set character amount number.value = value variable
+
+
+
+
 // When is male is selected, names that are predominatly male are displayed
 // When is female is selected, names that are predominatly female are displayed
 // When generate name button is clicked, a random name with the selected values is displayed in the text box
