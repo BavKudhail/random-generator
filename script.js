68713ab04c155ec3f40ce1d23c39bcfdb646ad4a
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-01-02 16:59:49 +0000

    Created javascript pseudo-code

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pseudo-code
+
+
+// When 'Number of Characters' range slider is adjusted. Then the number of characters display is synced accordingly. 
+// When is male is selected, names that are predominatly male are displayed
+// When is female is selected, names that are predominatly female are displayed
+// When generate name button is clicked, a random name with the selected values is displayed in the text box
